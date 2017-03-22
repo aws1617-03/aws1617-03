9b91c1e002dc3dd81230c57f38cabed3d7b25c76
Daniel Arteaga <dani8art.da@gmail.com>
2017-03-22 19:17:49 +0100

    fixed #1

---
+++ src/controllers/groups-controllers.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    getAll: _getAll,
+    getOneByName: _getOneByName,
+    create: _create,
+    update: _update,
+    deleteOne: _deleteOne,
+    deleteAll: _deleteAll
+};
+
+// GET all
+function _getAll(req, res, next) {
+
+}
+// GET one
+
+// POST one
+
+// PUT one
+
+// DELETE all
+
+// DELETE one
