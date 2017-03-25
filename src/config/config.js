4d797121a568561503d26e392e7c53132c51a7a5
Daniel Arteaga <dani8art.da@gmail.com>
2017-03-25 12:35:25 +0100

    fixed #4

---
+++ src/config/config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+
+module.exports = {
+    addConfiguration: _addConfiguration
+};
+
+function _addConfiguration(file) {
+    console.log('Add configuration from %s', file);
+    try {
+
+        var newConfig = jsyaml.safeLoad(fs.readFileSync(file))[process.env.ENV || 'development'];
+
+        for (var c in newConfig) {
+            module.exports[c] = newConfig[c];
+        }
+        console.log('Configuration has been added');
+
+    } catch (e) {
+        throw new Error('Error adding configuration ' + e.toString());
+    }
+}
