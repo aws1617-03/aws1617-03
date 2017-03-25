562cd4f8856e637ecd936d2853f13351aa9e26ba
Daniel Arteaga <dani8art.da@gmail.com>
2017-03-25 12:57:22 +0100

    fixed #5

---
+++ tests/config.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Config module tests', function () {
+    var config = require('../src/config/config');
+
+    it('exports addConfiguration', function () {
+        expect(config.addConfiguration instanceof Function);
+    });
+
+    it('add configuration', function () {
+        config.addConfiguration('tests/sources/test-configuration.json');
+
+        expect(config.port).to.be.equal(3000);
+        expect(config.api.basePath).to.be.equal("/api/v1");
+        expect(config.database.host).to.be.equal("localhost");
+        expect(config.database.port).to.be.equal("27017");
+        expect(config.database.name).to.be.equal("groups_db");
+        expect(config.database.collections[0].name).to.be.equal("groups");
+        expect(config.database.collections[0].schema).to.be.equal("./groups-schema.json");
+    });
+});
