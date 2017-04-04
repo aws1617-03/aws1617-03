5df92ba6138ee27c9014ab683e6612c404d2f316
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2017-04-04 00:12:16 +0000

    Added test group-controllers API

--- tests/config.test.js
+++ tests/config.test.js
@@ -15,7 +15,7 @@ describe('Config module tests', function () {
 
         expect(config.port).to.be.equal(3000);
         expect(config.api.basePath).to.be.equal("/api/v1");
-        expect(config.database.host).to.be.equal("192.168.99.100");
+        expect(config.database.host).to.be.equal("localhost");
         expect(config.database.port).to.be.equal("27017");
         expect(config.database.name).to.be.equal("groups_db");
         expect(config.database.collections[0].name).to.be.equal("groups");
--- tests/controllers/groups-controllers.test.js
+++ tests/controllers/groups-controllers.test.js
@@ -9,6 +9,44 @@ describe('Groups Controllers Tests', function () {
 
     before(function (done) {
         server.deploy(done);
+          
+        request.post({
+            url: 'http://localhost:3000/api/v1/groups',
+            json: true,
+            body: {
+                name: "ISA-tests1"
+            }
+        }, function (err, res) {
+            if (err || res.statusCode != 201) {
+                done(err);
+            } else {
+                request.post({
+                    url: 'http://localhost:3000/api/v1/groups',
+                    json: true,
+                    body: {
+                        name: "ISA-tests2"
+                    }
+                }, function (err, res) {
+                    if (err || res.statusCode != 201) {
+                        done(err);
+                    } else {
+                        request.post({
+                            url: 'http://localhost:3000/api/v1/groups',
+                            json: true,
+                            body: {
+                                name: "ISA-tests3"
+                            }
+                        }, function (err, res) {
+                            if (err || res.statusCode != 201) {
+                                done(err);
+                            } else {
+                                done();
+                            }
+                        });
+                    }
+                });
+            }
+        });
     });
 
     after(function (done) {
@@ -23,6 +61,20 @@ describe('Groups Controllers Tests', function () {
                 name: "ISA-tests"
             }
         }, function (err, res) {
+            if (err || res.statusCode != 201) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+    it('GET ALL /groups', function (done) {
+        request.getAll({
+            url: 'http://localhost:3000/api/v1/groups/',
+            json: true,
+
+        }, function (err, res) {
             if (err || res.statusCode != 200) {
                 done(err);
             } else {
@@ -30,5 +82,98 @@ describe('Groups Controllers Tests', function () {
             }
         });
     });
+    
+    
+    it('UPDATE /groups', function (done) {
+        request.update({
+            url: 'http://localhost:3000/api/v1/groups/ISA-tests',
+            json: true,
+            body: {
+                name: "ISAUpdate-tests"
+            }
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+
+    it('GET /groups', function (done) {
+        request.getOneByName({
+            url: 'http://localhost:3000/api/v1/groups/ISAUpdate-tests',
+            json: true,
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+    
+     it('DELETE /groups', function (done) {
+        request.deleteOne({
+            url: 'http://localhost:3000/api/v1/groups/ISAUpdate-tests',
+            json: true,
+            body: {
+              
+            }
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+    it('GET ALL /groups', function (done) {
+        request.getAll({
+            url: 'http://localhost:3000/api/v1/groups/',
+            json: true,
+
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+    it('DELETE ALL /groups', function (done) {
+        request.deleteAll({
+            url: 'http://localhost:3000/api/v1/groups/ISAUpdate-tests',
+            json: true,
+            body: {
+              
+            }
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
+    it('GET ALL /groups', function (done) {
+        request.getAll({
+            url: 'http://localhost:3000/api/v1/groups/',
+            json: true,
+
+        }, function (err, res) {
+            if (err || res.statusCode != 200) {
+                done(err);
+            } else {
+                done();
+            }
+        });
+    });
+    
 
 });
