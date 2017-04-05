1c4c3fcd6bec1ecde2e2c13a8ef69c430f124a40
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2017-04-05 01:00:44 +0000

    Added Administration UI over Groups resource

--- public/angular-app/groups/groups-controller.js
+++ public/angular-app/groups/groups-controller.js
@@ -16,5 +16,37 @@ angular.module("groups-app").controller("groupsCtl", function ($scope, $http) {
         
     };
     
+    $scope.deleteGroup = function(group) {
+            $http.delete('/api/v1/groups/' + group.name).then((response) => {
+                refresh();
+        });
+    };
+    
+   $scope.updateGroup = function() {
+            $http.put('/api/v1/groups/' + $scope.group.name, $scope.newGroup).then((response) => {
+                
+                refresh();
+                $('#myModal').modal('hide');
+            });
+        };
+
+    $scope.openModal = function(group) {
+        if (group) {
+            $scope.group = group;
+            $('#update').removeClass('hidden');
+            $('#add').addClass('hidden');
+             $scope.newGroup;
+        } else {
+            $('#update').addClass('hidden');
+            $('#add').removeClass('hidden');
+            $scope.group = {
+                name: 'FuffyBot',
+                description: 'defect',
+                members: ['Bot', 'Danger']
+            };
+             $scope.newGroup;
+        }
+    };
+    
     refresh();
 });
