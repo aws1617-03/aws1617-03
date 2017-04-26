8518d22495924752c698d492b71df8ae578b538a
Daniel Arteaga <dani8art.da@gmail.com>
2017-04-27 01:38:16 +0200

    Home finished

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,21 +1,25 @@
 'use strict';
 
 angular.module("groups-app", ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    .config(function ($httpProvider, $stateProvider, $urlRouterProvider) {
 
         $urlRouterProvider.otherwise("/");
 
+        var token = "84829f0084de9729788e23f5cc468408811f57d6";
+        $httpProvider.defaults.headers.common.Authorization = 'token ' + token;
 
         $stateProvider
             .state('site', {
                 abstract: true,
-                views: {'navbar@': {
-                    templateUrl: 'angular-app/navbar/navbar-template.html',
-                    controller: 'navbarCtl'
-                },'footer@': {
-                    templateUrl: 'angular-app/footer/footer-template.html',
-                    controller: 'footerCtl'
-                }}
+                views: {
+                    'navbar@': {
+                        templateUrl: 'angular-app/navbar/navbar-template.html',
+                        controller: 'navbarCtl'
+                    }, 'footer@': {
+                        templateUrl: 'angular-app/footer/footer-template.html',
+                        controller: 'footerCtl'
+                    }
+                }
 
             }).state('home', {
 
