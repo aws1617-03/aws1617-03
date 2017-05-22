f4664f3598616f538d7726d0e6a1979bd3a87dd6
Daniel Arteaga <dani8art.da@gmail.com>
2017-05-23 01:09:13 +0200

    added researches.service and first steps in views

--- public/angular-app/groups-id/groups-id-controller.js
+++ public/angular-app/groups-id/groups-id-controller.js
@@ -1,7 +1,24 @@
 'use strict';
 
-angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateParams) {
+angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateParams, $http, researchersService) {
 
     $scope.id = $stateParams.id;
+
+    $scope.refresh = function () {
+        $http.get('api/v1/groups/' + $scope.id).then(function (response) {
+            $scope.group = response.data;
+            // GET members
+            researchersService.getResearchersByGroups($scope.group._id).then(function (researchers) {
+                $scope.researchers = researchers;
+            }, function (error) {
+                console.log(error);
+                if (error.code === 404) {
+                    $scope.researchers = [];
+                }
+            });
+        });
+    };
+
+    $scope.refresh();
 
 });
