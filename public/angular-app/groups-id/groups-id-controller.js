275500d125b79cb69acc9afc26e11212e7b41ae1
Daniel Arteaga <darteaga@us.es>
2017-05-23 21:34:33 +0200

    added gchart.service

--- public/angular-app/groups-id/groups-id-controller.js
+++ public/angular-app/groups-id/groups-id-controller.js
@@ -1,10 +1,6 @@
 'use strict';
 
-angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateParams, $http, researchersService, elsevierService, $q, $timeout) {
-
-    google.charts.load('current', {
-        packages: ['corechart', 'bar', 'geochart']
-    });
+angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateParams, $http, researchersService, elsevierService, $q, $timeout, gchartService) {
 
     $scope.id = $stateParams.id;
 
@@ -81,17 +77,15 @@ angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateP
 
     }
 
-    //elsevierService.getPublicationsPerAgeByResercher("0000-0001-9827-1834");
-
     $scope.refresh().then(function (results) {
         $scope.group = results.group;
         $scope.researchers = results.researchers;
         $scope.loading = false;
         $timeout(function () {
-            drawPublicationsPerYears(results.yearsCount);
-            drawPublicationTypes(results.documentsTypesCount);
-            drawCitesPerYears(results.yearsCount);
-            drawCollaborationMap(results.collaborations);
+            gchartService.drawPublicationsPerYears(results.yearsCount);
+            gchartService.drawPublicationTypes(results.documentsTypesCount);
+            gchartService.drawCitesPerYears(results.yearsCount);
+            gchartService.drawCollaborationMap(results.collaborations);
         }, 500);
 
     }, function (error) {
@@ -99,118 +93,4 @@ angular.module("groups-app").controller("groupsIdCtl", function ($scope, $stateP
         $scope.researchers = [];
     });
 
-
-    //CHARTS FUNCTIONS
-
-    function drawCollaborationMap(collaborations) {
-        var counts = {};
-        collaborations.forEach(function (element) {
-            counts[element['affiliation-country']] = counts[element['affiliation-country']] ? counts[element['affiliation-country']] + 1 : 1;
-        });
-        console.log(counts);
-        var array = [
-            ['Country', 'Collaborations']
-        ];
-
-        for (var aff in counts) {
-            array.push([aff, counts[aff]]);
-        }
-        var data = google.visualization.arrayToDataTable(array);
-
-        var options = {};
-
-        var chart = new google.visualization.GeoChart(document.getElementById('collaborations_chart'));
-
-        chart.draw(data, options);
-    }
-
-    function drawPublicationsPerYears(yearsCount) {
-        var array = [
-            ['Year', 'Count']
-        ];
-        for (var y in yearsCount) {
-            array.push([y, yearsCount[y].count]);
-        }
-        var data = google.visualization.arrayToDataTable(array);
-
-        var options = {
-            chartArea: {
-                width: '70%'
-            },
-            hAxis: {
-                minValue: 0,
-                textStyle: {
-                    bold: true,
-                    fontSize: 12,
-                    color: '#4d4d4d'
-                }
-            },
-            vAxis: {
-                textStyle: {
-                    fontSize: 14,
-                    bold: true,
-                    color: '#848484'
-                }
-            }
-        };
-        var chart = new google.visualization.BarChart(document.getElementById('pub_per_year'));
-        chart.draw(data, options);
-    }
-
-    function drawCitesPerYears(yearsCount) {
-        var array = [
-            ['Year', 'Cites']
-        ];
-        for (var y in yearsCount) {
-            array.push([y, yearsCount[y].citations]);
-        }
-        var data = google.visualization.arrayToDataTable(array);
-
-        var options = {
-            chartArea: {
-                width: '70%'
-            },
-            colors: ['red'],
-            hAxis: {
-                minValue: 0,
-                textStyle: {
-                    bold: true,
-                    fontSize: 12,
-                    color: '#4d4d4d'
-                }
-            },
-            vAxis: {
-                textStyle: {
-                    fontSize: 14,
-                    bold: true,
-                    color: '#848484'
-                }
-            }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('cites_per_year'));
-        chart.draw(data, options);
-    }
-
-    function drawPublicationTypes(documentsTypesCount) {
-        var array = [
-            ['Type', 'Count']
-        ];
-        for (var t in documentsTypesCount) {
-            array.push([t, documentsTypesCount[t]]);
-        }
-        var data = google.visualization.arrayToDataTable(array);
-
-        var options = {
-            pieHole: 0.3,
-            chartArea: {
-                left: 20,
-                top: 10,
-                width: '90%',
-                height: '90%'
-            }
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('type_of_pub'));
-        chart.draw(data, options);
-    }
 });
